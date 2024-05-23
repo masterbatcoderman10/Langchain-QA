336136cbcf344703fd8f106a4cb1ba51fa5501d2
masterbatcoderman10 <suhaib.athar@gmail.com>
2024-05-23 17:19:47 +0400

    Added history tracking in Web UI

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,32 @@
 import streamlit as st
 from scripts.rag import get_answer, store
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.messages import HumanMessage, AIMessage
 import uuid
+from pprint import pprint
 
 st.title('QA With ChatGPT')
 
 #New conversation
+session_id = "123456789"
 if "store" not in st.session_state:
     #Agent's history is stored in the session state this is to contextualize the conversation
-    st.session_state.store = {}
+    st.session_state.store = {
+        session_id: InMemoryChatMessageHistory()
+    }
+
+    pprint(st.session_state.store)
+
+#Add old messages
+for message in st.session_state.store[session_id].messages:
+    
+    if isinstance(message, HumanMessage):
+        with st.chat_message("user"):
+            st.markdown(message.content)
+    
+    if isinstance(message, AIMessage):
+        with st.chat_message("assistant"):
+            st.markdown(message.content)
 
 #Get user input
 if prompt := st.chat_input("Type a message!"):
@@ -17,6 +36,7 @@ if prompt := st.chat_input("Type a message!"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        respnse = st.write_stream(get_answer(prompt, session_id=uuid.uuid4().hex))
+        respnse = st.write_stream(get_answer(prompt, session_id=session_id))
     
-    print(store)
+    st.session_state.store[session_id] = store[session_id]
+

0fedbe4cf44606120acacb9495186977ce6b25b3
masterbatcoderman10 <suhaib.athar@gmail.com>
2024-05-23 14:40:04 +0400

    added history tracking and streaming

--- scripts/rag.py
+++ scripts/rag.py
@@ -7,36 +7,104 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
 
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-template = """
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+
+qa_system_prompt = """You are an assistant for question-answering tasks. \
 - Use the following pieces of context to answer the question at the end.
 - If you don't know the answer, just say that you don't know, don't try to make up an answer.
 - Ensure you provide a detailed and helpful answer.
 - Always say "thanks for asking!" at the end of the answer.
 
-{context}
+{context}"""
 
-Question: {question}
-
-Helpful Answer:"""
-
-prompt = PromptTemplate.from_template(template)
-
-load_dotenv()
 
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
+# prompt for tracking history - subchain
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+# qa prompt
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
 )
 
-for chunk in rag_chain.stream("What is electronic waste?"):
-    print(chunk, end="", flush=True)
+# Subchain that contextualizes the question with the chat history
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(
+    history_aware_retriever, question_answer_chain)
+
+### Statefully manage chat history ###
+store = {}
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+
+def get_answer(question: str, session_id: str = None):
+    if session_id is None:
+        session_id = "abc123"
+    # return conversational_rag_chain.invoke(
+    #     {"input": question},
+    #     config={
+    #         "configurable": {"session_id": session_id}
+    #     },  # constructs a key "abc123" in `store`.
+    # )["answer"]
+    for chunk in conversational_rag_chain.stream(
+        {"input": "What is electronic waste?"},
+        config={"configurable": {"session_id": "abc123"}},
+    ):
+        if 'answer' in chunk:
+            yield chunk['answer']
+
+
+# rag_chain = (
+#     {"context": retriever | format_docs, "question": RunnablePassthrough()}
+#     | prompt
+#     | llm
+#     | StrOutputParser()
+# )
+# for chunk in rag_chain.stream("What is electronic waste?"):
+#     print(chunk, end="", flush=True)
